b6bb2790a02a06e88a32570a5ddec691c52b688b
claudiulodro <claudiulodro@gmail.com>
2018-10-01 21:16:07 -0400

    v1.0

--- hello.php
+++ hello.php
@@ -1,66 +1,25 @@
 <?php
 /**
- * @package Hello_Dolly
- * @version 1.6
+ * @package Hello_Death
+ * @version 1.0
  */
 /*
-Plugin Name: Hello Dolly
-Plugin URI: http://wordpress.org/extend/plugins/hello-dolly/
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
-Author: Matt Mullenweg
-Version: 1.6
-Author URI: http://ma.tt/
+Plugin Name: Hello Death
+Plugin URI: 
+Description: The Bhutanese say think about death 5 times a day and you will be happy. Now you can do that while managing your WordPress site.
+Author: Matt Mullenweg, Claudiu Lodromanean
+Version: 1.0
 */
 
-function hello_dolly_get_lyric() {
-	/** These are the lyrics to Hello Dolly */
-	$lyrics = "Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-So, take her wrap, fellas
-Find her an empty lap, fellas
-Dolly'll never go away again
-Hello, Dolly
-Well, hello, Dolly
-It's so nice to have you back where you belong
-You're lookin' swell, Dolly
-I can tell, Dolly
-You're still glowin', you're still crowin'
-You're still goin' strong
-We feel the room swayin'
-While the band's playin'
-One of your old favourite songs from way back when
-Golly, gee, fellas
-Find her a vacant knee, fellas
-Dolly'll never go away
-Dolly'll never go away
-Dolly'll never go away again";
-
-	// Here we split it into lines
-	$lyrics = explode( "\n", $lyrics );
-
-	// And then randomly choose a line
-	return wptexturize( $lyrics[ mt_rand( 0, count( $lyrics ) - 1 ) ] );
-}
-
-// This just echoes the chosen line, we'll position it later
-function hello_dolly() {
-	$chosen = hello_dolly_get_lyric();
+function hello_death() {
+	$chosen = __( 'You are going to die.', 'hello-death' );
 	echo "<p id='dolly'>$chosen</p>";
 }
 
-// Now we set that function up to execute when the admin_notices action is called
-add_action( 'admin_notices', 'hello_dolly' );
+add_action( 'admin_notices', 'hello_death' );
 
 // We need some CSS to position the paragraph
-function dolly_css() {
+function death_css() {
 	// This makes sure that the positioning is also good for right-to-left languages
 	$x = is_rtl() ? 'left' : 'right';
 
@@ -77,6 +36,6 @@ function dolly_css() {
 	";
 }
 
-add_action( 'admin_head', 'dolly_css' );
+add_action( 'admin_head', 'death_css' );
 
 ?>
--- readme.txt
+++ readme.txt
@@ -1,11 +1,11 @@
-=== Hello Dolly ===
-Contributors: matt
+=== Hello Death ===
+Contributors: matt, claudiulodro
 Requires at least: 3.0
 Stable tag: 1.6
 Tested up to: 4.1
 
-This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong.
+The Bhutanese say think about death 5 times a day and you will be happy. Now you can do that while managing your WordPress site.
 
 == Description ==
 
-This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+The Bhutanese say think about death 5 times a day and you will be happy. Now you can do that while managing your WordPress site. When activated you will see <cite>You are going to die.</cite> in the upper right of your admin screen on every page.
